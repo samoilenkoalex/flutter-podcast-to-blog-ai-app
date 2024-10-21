190d209ec79ffec8da9f2b534f9bfb3680d8be75
Oleksandr Samoilenko <oleksandr.samoilenko@extrawest.com>
2024-10-21 17:34:22 +0300

    add methods to a api calls

--- mobile/lib/services/api_service.dart
+++ mobile/lib/services/api_service.dart
@@ -1,6 +1,9 @@
 import 'dart:convert';
+import 'dart:developer';
+import 'dart:io';
 
 import 'package:http/http.dart' as http;
+import 'package:path_provider/path_provider.dart';
 
 import '../features/podcast/models/podcast_model.dart';
 
@@ -27,5 +30,83 @@ class ApiService {
       result = 'Failed to fetch summary';
     }
     return result;
+  }
+
+  Future<String> getEpisodeSummary({
+    required String id,
+  }) async {
+    dynamic result = [];
+    final response = await http.post(
+      Uri.parse('$baseUrl/episodeSummarize'),
+      headers: <String, String>{
+        'Content-Type': 'application/json; charset=UTF-8',
+      },
+      body: jsonEncode(<String, String>{
+        'searchTerm': id,
+      }),
+    );
+
+    if (response.statusCode == 200) {
+      result = response.body;
+    } else {
+      result = 'Failed to fetch summary';
+    }
+    return result;
+  }
+
+  Future<String> getSpeechToText({
+    required String id,
+  }) async {
+    dynamic result = [];
+    final response = await http.post(
+      Uri.parse('$baseUrl/episodeSpeechToText'),
+      headers: <String, String>{
+        'Content-Type': 'application/json; charset=UTF-8',
+      },
+      body: jsonEncode(<String, String>{
+        'searchTerm': id,
+      }),
+    );
+
+    if (response.statusCode == 200) {
+      result = response.body;
+    } else {
+      result = 'Failed to fetch summary';
+    }
+    return result;
+  }
+
+  Future<List<int>> getAudio({required String summary}) async {
+    final response = await http.post(
+      Uri.parse('$baseUrl/summarizedAudio'),
+      headers: <String, String>{
+        'Content-Type': 'application/json; charset=UTF-8',
+      },
+      body: jsonEncode(<String, String>{
+        'summaryText': summary,
+      }),
+    );
+
+    if (response.statusCode == 200) {
+      log('Response Content-Type: ${response.headers['content-type']}');
+      log('Response body length: ${response.bodyBytes.length}');
+
+      if (response.headers['content-type'] != 'audio/mpeg') {
+        throw Exception('Unexpected content type: ${response.headers['content-type']}');
+      }
+
+      return response.bodyBytes;
+    } else {
+      throw Exception('Failed to fetch audio: ${response.statusCode}');
+    }
+  }
+
+  Future<String> saveRawResponse(List<int> data) async {
+    final directory = await getApplicationDocumentsDirectory();
+    final path = directory.path;
+    final file = File('$path/raw_response.mp3');
+    await file.writeAsBytes(data);
+    log('Raw response saved to: ${file.path}');
+    return file.path;
   }
 }
